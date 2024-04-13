239632331e5345cd46b8d491fa09091946fb2d7c
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-04-13 12:17:36 +0530

    Added the header section

--- marketplace-website/src/pages/Main.jsx
+++ marketplace-website/src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './Main.css';
 import Sidemenu from "../components/Sidemenu";
+import Header from "./Header";
 
 function Main(){
     return(
         <main>
             <Sidemenu/>
             <div className="banner"></div>
+            <Header/>
         </main>
     )
 }
